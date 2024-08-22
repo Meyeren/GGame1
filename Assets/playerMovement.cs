2472f457be16e9524b00996cae9309bd978b97cb
Meyeren <lucasmeyergaming@gmail.com>
2024-08-22 13:00:05 +0200

    Revert "Merge branch 'main' of https://github.com/Meyeren/GGame1"
    This reverts commit f04a0265bb4fb8ff4e69c7e05c20c40448ac079a, reversing
    changes made to 1e9f7d1ac8aa3fe824bd765379bf2c4da97ad14e.

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,46 +4,15 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-
-    private RigidBody2D rb;
-    public float speed = 3f;
-    private float inputX;
-
-    private float jumpForce = 16f;
-    private bool isFacingRight = true;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float checkRadius = 0.2f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private bool isGrounded;
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputX = Input.GetAxisRaw("Horizontal");
-
-        //Vender karakteren den vej man bevæger sig
-        if (inputX < 0 && isFacingRight)
-        {
-            flip()
-        }
-        if (inputX > 0 && !isFacingRight)
-        {
-            flip()
-        }
+        
     }
-
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 tempScale = transform.localScale;
-        tempScale.x *= -1;
-        transform.localScale = tempScale;
-    }
+}
